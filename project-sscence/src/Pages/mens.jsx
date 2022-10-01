22634a38e066b789053e2d22f15c903400d99461
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2022-10-01 17:42:29 +0530

    adding mens and womens page

--- project-sscence/src/Pages/mens.jsx
+++ project-sscence/src/Pages/mens.jsx
@@ -1,4 +1,53 @@
+import {
+  Text,
+  Button,
+  InputGroup,
+  InputRightElement,
+  Input,
+  Img,
+  SimpleGrid,
+  Flex,
+  Box
+} from "@chakra-ui/react";
+import React from "react";
+// import { useContext } from "react";
+// import { AuthContext } from "../Context/AuthContext";
+import axios from "axios";
+import { Link, Navigate } from "react-router-dom";
+
 export function Mens() {
-    return <div>hello mens</div>;
-  }
-  
+  let [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(
+        "https://pacific-refuge-88537.herokuapp.com/api/clothing?category=Mens"
+      )
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(data);
+  return (
+    <Box>
+      <SimpleGrid columns={[2, null, 3]} spacing="40px">
+        {data?.map((el) => {
+          return (
+            <Box heigth="full" key={Date.now().toString()}>
+              <Img heigth="50%" src={el.images[1]}></Img>
+              <Text>{el.title}</Text>
+              <Text>{el.subtitle}</Text>
+              <Text>Price : ${el.discounted_price}</Text>
+              <Text>Rating : {el.rating}</Text>
+            </Box>
+          );
+        })}
+      </SimpleGrid>
+    </Box>
+  );
+}
+// Passing `columns={[2, null, 3]}` and `columns={{sm: 2, md: 3}}`
+// will have the same effect.
--- project-sscence/src/Pages/womens.jsx
+++ project-sscence/src/Pages/womens.jsx
@@ -1,4 +1,57 @@
 export function Womens() {
     return <div>hello Womens</div>;
   }
+  import {
+    Text,
+    Button,
+    InputGroup,
+    InputRightElement,
+    Input,
+    Img,
+    SimpleGrid,
+    Flex,
+    Box
+  } from "@chakra-ui/react";
+  import React from "react";
+  // import { useContext } from "react";
+  // import { AuthContext } from "../Context/AuthContext";
+  import axios from "axios";
+  import { Link, Navigate } from "react-router-dom";
+  
+  export function Womens() {
+    let [data, setData] = React.useState([]);
+  
+    React.useEffect(() => {
+      axios
+        .get(
+          "https://pacific-refuge-88537.herokuapp.com/api/clothing?category=Womens"
+        )
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+    console.log(data);
+    return (
+      <Box>
+        <SimpleGrid columns={[2, 3, 4]} spacing="40px">
+          {data?.map((el) => {
+            return (
+              <Box heigth="full" key={Date.now().toString()}>
+                <Img heigth="50%" src={el.images[0]}></Img>
+                <Text>{el.title}</Text>
+                <Text>{el.subtitle}</Text>
+                <Text>Price : ${el.discounted_price}</Text>
+                <Text>Rating : {el.rating}</Text>
+              </Box>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
+    );
+  }
+  // Passing `columns={[2, null, 3]}` and `columns={{sm: 2, md: 3}}`
+  // will have the same effect.
   
